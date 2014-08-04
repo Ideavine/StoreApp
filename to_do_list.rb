6d50d1abdfd7fa8e457b1a4fdd66c64222ffb24b
Christopher Mehmed <chrismehmed@gmail.com>
2014-08-03 21:46:14 -0400

    First attempt at a Fork, with a to do list

---
+++ to_do_list.rb
@@ -0,0 +1,46 @@
+To Do List, for Rails Project: Shopper
+
+1. 	Add to the list of things we need to do to get the project working properly.
+
+2. 	If you want to work on something, in the to do list, add you initials to the begining of the item:
+	CM - Eat all the Taco Beans.
+
+3. 	Once you finish an item off the list, cut and paste it below the Completed section.
+
+
+Primary:
+
+1.	user/:id/edit
+	for the User edit action
+	and edit.html.erb
+
+2.	
+
+3.	
+
+
+Secondary:
+
+1.	
+
+2.	
+
+3.	
+
+
+Elective:
+
+1.	Add a CSS file, and begin styling
+
+2.
+
+3.
+
+
+=begin
+Completed:
+
+
+
+	
+=end
